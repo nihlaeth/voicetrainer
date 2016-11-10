da1af0c3f5efd460a84780d9fa2e28941fc6947f
nihlaeth <info@nihlaeth.nl>
2016-11-11 00:07:44 +0100

    plumbing

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='voicetrainer',
+    version='1.0',
+    description='midi voice exercises',
+    author='nihlaeth',
+    author_email='info@nihlaeth.nl',
+    packages=['voicetrainer'])
