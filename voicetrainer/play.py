249554b4fcb2fbafe8ac0e9c70481fa5012cc6a6
nihlaeth <info@nihlaeth.nl>
2016-11-11 14:59:22 +0100

    untested: midi playback

---
+++ voicetrainer/play.py
@@ -0,0 +1,46 @@
+"""Play midi via fluidsynth."""
+from typing import Callable
+from os.path import dirname, join, realpath
+from asyncio import create_subprocess_exec, sleep, get_event_loop
+from asyncio.subprocess import PIPE, Process
+
+async def get_qsynth_port() -> str:
+    """Check on wich midi port qsynth is running, retry every 5 seconds."""
+    while True:
+        result = await create_subprocess_exec(
+            'pmidi', '-l', stdout=PIPE, stderr=PIPE)
+        output, err = await result.communicate()
+        if result.returncode != 0:
+            raise OSError('pmidi not functioning {}'.format(err))
+        output = output.split('\n')
+        for line in output:
+            if 'Qsynth' in line or 'qsynth' in line:
+                # extract port
+                return line.strip().split(' ')[0]
+        await sleep(5)
+
+async def play_midi(port: str, midi: str) -> Process:
+    """Start playing midi file."""
+    return await create_subprocess_exec(
+        'pmidi', '-p', port, midi)
+
+async def stop_midi(proc: Process) -> None:
+    """Stop midi playback."""
+    proc.terminate()
+
+async def exec_on_midi_end(proc: Process, func: Callable) -> int:
+    """Exec func when midi stops playing."""
+    return_code = await proc.wait()
+    func()
+    return return_code
+
+if __name__ == "__main__":
+    # pylint: disable=invalid-name
+    loop = get_event_loop()
+    port_ = loop.run_until_complete(get_qsynth_port())
+    proc_ = loop.run_until_complete(play_midi(
+        port_,
+        join(
+            dirname(realpath(__file__)),
+            "../exercises/control-140bmp-d.midi")))
+    loop.run_until_complete(exec_on_midi_end(proc_, lambda: print('finis')))
