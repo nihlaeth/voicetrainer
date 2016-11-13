6556176002dbf6775b5452139e522ba9a6a6754e
nihlaeth <info@nihlaeth.nl>
2016-11-13 14:47:37 +0100

    debut play_midi

--- voicetrainer/play.py
+++ voicetrainer/play.py
@@ -23,7 +23,7 @@ async def get_qsynth_port() -> str:
 async def play_midi(port: str, midi: str) -> Process:
     """Start playing midi file."""
     return await create_subprocess_exec(
-        'pmidi', '-p', port, midi)
+        'pmidi', '-p', port, midi.encode('utf-8'))
 
 async def stop_midi(proc: Process) -> None:
     """Stop midi playback."""
