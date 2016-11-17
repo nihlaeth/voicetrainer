17c9812b75b3f2e337597d412a3b68186530a3ea
nihlaeth <info@nihlaeth.nl>
2016-11-17 14:40:42 +0100

    refactor gui element storage

--- voicetrainer/gui.py
+++ voicetrainer/gui.py
@@ -57,28 +57,23 @@ class Application(tk.Tk):
         self.notebook = None
         self.resize = None
         self.tabs = []
-        self.bpm = []
-        self.labels = []
-        self.control_vars = []
-        self.scrolling = []
-        self.pitches = []
-        self.sheets = []
-        self.controls = []
         self.image_cache = {}
         self.create_widgets()
         self.updater(interval)
 
-    def create_tab(self, tab: ttk.Frame, tab_num: int) -> None:
+    def create_tab(self, exercise: str) -> None:
         """Populate exercise tab."""
-        # init storage dicts
-        self.labels.append({})
-        self.control_vars.append({})
-        self.scrolling.append({})
-        self.controls.append({})
+        tab = ttk.Frame(self.notebook)
+        tab.rowconfigure(1, weight=1)
+        tab.columnconfigure(3, weight=1)
+        self.notebook.add(tab, text=exercise)
+        self.tabs.append({})
+        tab_num = len(self.tabs) - 1
+        self.tabs[tab_num]['tab'] = tab
 
         # bpm selector
         bpm_label = ttk.Label(tab, text="bpm:")
-        self.labels[tab_num]['bpm'] = bpm_label
+        self.tabs[tab_num]['bpm_label'] = bpm_label
         bpm_label.grid(column=2, row=0, sticky=tk.N+tk.W)
         bpm = tk.Scale(
             tab,
@@ -90,12 +85,12 @@ class Application(tk.Tk):
             resolution=10,
             orient=tk.HORIZONTAL)
         bpm.set(140)
-        self.bpm.append(bpm)
+        self.tabs[tab_num]['bpm'] = bpm
         bpm.grid(column=3, row=0, sticky=tk.W+tk.N)
 
         # pitch selector
         scrollbar = tk.Scrollbar(tab, orient=tk.VERTICAL)
-        self.scrolling[tab_num]['pitch'] = scrollbar
+        self.tabs[tab_num]['pitch_scrollbar'] = scrollbar
         scrollbar.grid(row=1, column=0, sticky=tk.N+tk.S+tk.W)
 
         listbox = tk.Listbox(
@@ -105,7 +100,7 @@ class Application(tk.Tk):
             # make selections in multiple listboxes possible
             exportselection=False,
             selectmode=tk.MULTIPLE)
-        self.pitches.append(listbox)
+        self.tabs[tab_num]['pitches'] = listbox
         listbox.grid(row=1, column=1, sticky=tk.N+tk.S+tk.W)
         scrollbar['command'] = listbox.yview
 
@@ -114,73 +109,73 @@ class Application(tk.Tk):
 
         # controls
         frame = ttk.Frame(tab)
-        self.controls[tab_num]['frame'] = frame
+        self.tabs[tab_num]['control_frame'] = frame
         frame.grid(column=2, row=2, columnspan=2, sticky=tk.W+tk.N)
         # sound pitch random autonext repeat_once next play/stop
 
         soundvar = tk.StringVar()
         soundvar.set("Mi")
-        self.control_vars[tab_num]['sound'] = soundvar
+        self.tabs[tab_num]['sound'] = soundvar
         sound = tk.OptionMenu(
             frame,
             soundvar,
             *self.sound_list,
             command=lambda _: asyncio.ensure_future(self.update_sheet()))
-        self.controls[tab_num]['sound'] = sound
+        self.tabs[tab_num]['sound_menu'] = sound
         sound.grid(column=0, row=0, sticky=tk.W+tk.N)
 
         textvar = tk.StringVar()
         textvar.set(self.pitch_list[listbox.curselection()[0]])
-        self.control_vars[tab_num]['curr_pitch'] = textvar
+        self.tabs[tab_num]['curr_pitch'] = textvar
         curr_pitch = tk.OptionMenu(
             frame,
             textvar,
             *self.pitch_list,
             command=lambda _: asyncio.ensure_future(
                 self.on_pitch_change()))
-        self.controls[tab_num]['pitch'] = curr_pitch
+        self.tabs[tab_num]['pitch_menu'] = curr_pitch
         curr_pitch.grid(column=1, row=0, sticky=tk.W+tk.N)
 
         rand_int = tk.IntVar()
-        self.control_vars[tab_num]['random'] = rand_int
+        self.tabs[tab_num]['random'] = rand_int
         random = ttk.Checkbutton(frame, text="random", variable=rand_int)
-        self.controls[tab_num]['random'] = random
+        self.tabs[tab_num]['random_box'] = random
         random.grid(column=2, row=0, sticky=tk.W+tk.N)
 
         auto_int = tk.IntVar()
-        self.control_vars[tab_num]['autonext'] = auto_int
+        self.tabs[tab_num]['autonext'] = auto_int
         autonext = ttk.Checkbutton(
             frame, text="autonext", variable=auto_int)
-        self.controls[tab_num]['autonext'] = autonext
+        self.tabs[tab_num]['autonext_box'] = autonext
         autonext.grid(column=3, row=0, sticky=tk.W+tk.N)
 
         repeat = ttk.Button(
             frame,
             text="repeat once",
             command=self.set_repeat_once)
-        self.controls[tab_num]['repeat'] = repeat
+        self.tabs[tab_num]['repeat'] = repeat
         repeat.grid(column=4, row=0, sticky=tk.W+tk.N)
 
         next_ = ttk.Button(
             frame,
             text="next",
             command=lambda: asyncio.ensure_future(self.next_()))
-        self.controls[tab_num]['next_'] = next_
+        self.tabs[tab_num]['next_'] = next_
         next_.grid(column=5, row=0, sticky=tk.W+tk.N)
 
         play_stop = tk.StringVar()
         play_stop.set("play")
-        self.control_vars[tab_num]['play_stop'] = play_stop
+        self.tabs[tab_num]['play_stop'] = play_stop
         play = ttk.Button(
             frame,
             textvariable=play_stop,
             command=lambda: asyncio.ensure_future(self.play_or_stop()))
-        self.controls[tab_num]['play'] = play
+        self.tabs[tab_num]['play'] = play
         play.grid(column=5, row=0, sticky=tk.W+tk.N)
 
         # sheet display
         sheet = ttk.Label(tab)
-        self.sheets.append(sheet)
+        self.tabs[tab_num]['sheet'] = sheet
         sheet.grid(column=2, row=1, columnspan=2, sticky=tk.N+tk.W)
         asyncio.ensure_future(self.update_sheet(tab_num=tab_num))
 
@@ -211,12 +206,7 @@ class Application(tk.Tk):
             if isfile(join(self.data_path, item)) and item.endswith('.ly'):
                 exercises.append(item[:-3])
         for ex in exercises:
-            tab = ttk.Frame(self.notebook)
-            tab.rowconfigure(1, weight=1)
-            tab.columnconfigure(3, weight=1)
-            self.notebook.add(tab, text=ex)
-            self.tabs.append(tab)
-            self.create_tab(tab, len(self.tabs) - 1)
+            self.create_tab(ex)
         self.notebook.grid(
             column=0, row=0, columnspan=3, sticky=tk.N+tk.S+tk.E+tk.W)
         self.compiler_label = ttk.Label(self, text="Compiler:")
@@ -264,11 +254,11 @@ Do you still want to exit? The task will be aborted."""):
         for i in range(len(self.tabs)):
             tab_name = self.notebook.tab(i)['text']
             data[tab_name] = {}
-            data[tab_name]['pitch_selection'] = self.pitches[i].curselection()
-            data[tab_name]['bpm'] = self.bpm[i].get()
-            data[tab_name]['sound'] = self.control_vars[i]['sound'].get()
-            data[tab_name]['autonext'] = self.control_vars[i]['autonext'].get()
-            data[tab_name]['random'] = self.control_vars[i]['random'].get()
+            data[tab_name]['pitch_selection'] = self.tabs[i]['pitches'].curselection()
+            data[tab_name]['bpm'] = self.tabs[i]['bpm'].get()
+            data[tab_name]['sound'] = self.tabs[i]['sound'].get()
+            data[tab_name]['autonext'] = self.tabs[i]['autonext'].get()
+            data[tab_name]['random'] = self.tabs[i]['random'].get()
         with open(join(self.data_path, 'state.json'), 'w') as st_file:
             st_file.write(json.dumps(data))
 
@@ -282,14 +272,15 @@ Do you still want to exit? The task will be aborted."""):
             tab_name = self.notebook.tab(i)['text']
             if tab_name not in data:
                 continue
-            self.pitches[i].selection_clear(0, len(self.pitch_list) - 1)
+            self.tabs[i]['pitches'].selection_clear(
+                0, len(self.pitch_list) - 1)
             for p_index in data[tab_name]['pitch_selection']:
-                self.pitches[i].selection_set(p_index)
-            self.bpm[i].set(data[tab_name]['bpm'])
-            self.control_vars[i]['sound'].set(data[tab_name]['sound'])
-            self.control_vars[i]['autonext'].set(
+                self.tabs[i]['pitches'].selection_set(p_index)
+            self.tabs[i]['bpm'].set(data[tab_name]['bpm'])
+            self.tabs[i]['sound'].set(data[tab_name]['sound'])
+            self.tabs[i]['autonext'].set(
                 data[tab_name]['autonext'])
-            self.control_vars[i]['random'].set(data[tab_name]['random'])
+            self.tabs[i]['random'].set(data[tab_name]['random'])
 
     def update_compiler(self):
         """Set compiler progress bar status."""
@@ -317,7 +308,7 @@ Do you still want to exit? The task will be aborted."""):
         png = await self.get_file(tab_num=tab_num)
         if png not in self.image_cache:
             self.image_cache[png] = tk.PhotoImage(file=png)
-        self.sheets[tab_num].config(image=self.image_cache[png])
+        self.tabs[tab_num]['sheet'].config(image=self.image_cache[png])
 
     async def on_pitch_change(self):
         """New pitch was picked by user or app."""
@@ -334,24 +325,24 @@ Do you still want to exit? The task will be aborted."""):
 
     async def next_(self):
         """Skip to next exercise."""
-        curr_pitch = self.control_vars[self.tab_num]['curr_pitch'].get()
+        curr_pitch = self.tabs[self.tab_num]['curr_pitch'].get()
         curr_pos = self.pitch_list.index(curr_pitch)
         pitch_pos = curr_pos
-        pitch_selection = self.pitches[self.tab_num].curselection()
+        pitch_selection = self.tabs[self.tab_num]['pitches'].curselection()
         if len(pitch_selection) == 0:
             return
-        if self.control_vars[self.tab_num]['random'].get() == 1:
+        if self.tabs[self.tab_num]['random'].get() == 1:
             while pitch_pos == curr_pos:
                 pitch_pos = choice(pitch_selection)
         elif curr_pos in pitch_selection:
             if pitch_selection.index(curr_pos) >= len(pitch_selection) - 1:
-                curr_sound = self.control_vars[self.tab_num]['sound'].get()
+                curr_sound = self.tabs[self.tab_num]['sound'].get()
                 sound_pos = self.sound_list.index(curr_sound)
                 if sound_pos < len(self.sound_list) - 1:
-                    self.control_vars[self.tab_num]['sound'].set(
+                    self.tabs[self.tab_num]['sound'].set(
                         self.sound_list[sound_pos + 1])
                 else:
-                    self.control_vars[self.tab_num]['sound'].set(
+                    self.tabs[self.tab_num]['sound'].set(
                         self.sound_list[0])
                 asyncio.ensure_future(self.update_sheet())
                 pitch_pos = pitch_selection[0]
@@ -363,7 +354,7 @@ Do you still want to exit? The task will be aborted."""):
                 pitch_pos += 1
                 if pitch_pos >= len(self.pitch_list):
                     pitch_pos = 0
-        self.control_vars[self.tab_num]['curr_pitch'].set(
+        self.tabs[self.tab_num]['curr_pitch'].set(
             self.pitch_list[pitch_pos])
         # I thought tkinter would call this, but apparently not
         await self.on_pitch_change()
@@ -382,9 +373,9 @@ Do you still want to exit? The task will be aborted."""):
         if tab_num is None:
             tab_num = self.tab_num
         tab_name = self.notebook.tab(tab_num)['text']
-        pitch = self.control_vars[tab_num]['curr_pitch'].get()
-        bpm = self.bpm[tab_num].get()
-        sound = self.control_vars[tab_num]['sound'].get()
+        pitch = self.tabs[tab_num]['curr_pitch'].get()
+        bpm = self.tabs[tab_num]['bpm'].get()
+        sound = self.tabs[tab_num]['sound'].get()
         extension = ".ly"
         if midi:
             file_name = join(
@@ -435,7 +426,7 @@ Do you still want to exit? The task will be aborted."""):
         except Exception as err:
             showerror("Could not start midi playback", str(err))
             raise
-        self.control_vars[self.tab_num]['play_stop'].set("stop")
+        self.tabs[self.tab_num]['play_stop'].set("stop")
         asyncio.ensure_future(exec_on_midi_end(
             self.player,
             self.on_midi_stop))
@@ -451,10 +442,10 @@ Do you still want to exit? The task will be aborted."""):
         if self.stopping:
             self.play_next = False
             self.stopping = False
-            self.control_vars[self.tab_num]['play_stop'].set("play")
+            self.tabs[self.tab_num]['play_stop'].set("play")
             return
         if self.play_next or \
-                self.control_vars[self.tab_num]['autonext'].get() == 1:
+                self.tabs[self.tab_num]['autonext'].get() == 1:
             self.play_next = False
             if not self.repeat_once:
                 await self.next_()
@@ -462,7 +453,7 @@ Do you still want to exit? The task will be aborted."""):
                 self.repeat_once = False
                 await self.play()
             return
-        self.control_vars[self.tab_num]['play_stop'].set("play")
+        self.tabs[self.tab_num]['play_stop'].set("play")
 
 def start():
     """Start gui and event loop."""
